78f487b0d457185d92d83bb107dd2fd6a194aac3
Mutlaq Aldhbuiub <matulkn@gmail.com>
2023-06-21 11:16:44 +0300

    Adding new image for adha

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 var canvas = document.getElementById("myCanvas");
 var context = canvas.getContext("2d");
 
-var imageWidth = 1966;
-var imageHeight = 3356;
+var imageWidth = 754;
+var imageHeight = 1311;
 
 var imageObj = new Image(imageWidth,imageHeight);
 
@@ -35,14 +35,14 @@ downloadCardButton.addEventListener('click', function(e){
 
     // custom font
     context.textAlign = 'center';
-    context.font = "80pt BrandoBold";
+    context.font = "28pt DINN";
 
     // text color
     context.fillStyle = '#273476';
 
     // center and make text
     textWidth = (imageWidth - imageWidth / 2)
-    textHeight = (imageHeight - 450)
+    textHeight = (imageHeight - 130)
     
     context.fillText(text, textWidth, textHeight);
 
@@ -50,7 +50,7 @@ downloadCardButton.addEventListener('click', function(e){
     document.getElementById('name').value = "";
 
     // download the image
-    DownloadCanvasAsImage();
+    // DownloadCanvasAsImage();
 });
 
 
